44a6ee0cfa08e1ebf9e0e47c02f34541b08ab38c
mohamed <mohamedibrahim1223@gmail.com>
2021-09-14 08:20:45 +0300

    Delivers success with no items on 200 http response with empty json list create FeedImageMapper to handle success response.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,11 +21,44 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success:
-				completion(.failure(Error.invalidData))
+			case let .success((data, response)):
+				completion(FeedImageMapper.map(data, from: response))
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
+	}
+}
+
+final class FeedImageMapper {
+	
+	private struct Root: Decodable {
+		let items: [Item]
+		var feed: [FeedImage] {
+			return items.map{ $0.item }
+		}
+	}
+	
+	private struct Item: Decodable {
+		let image_id: UUID
+		let image_desc: String?
+		let image_loc: String?
+		let image_url: URL
+		
+		var item: FeedImage {
+			return FeedImage(id: image_id,
+							 description: image_desc,
+							 location: image_loc,
+							 url: image_url)
+		}
+	}
+	
+	static func map(_ data: Data,from response: HTTPURLResponse)-> FeedLoader.Result {
+		guard response.statusCode == 200,
+			  let _ = try? JSONDecoder().decode(Root.self, from: data)
+		else {
+			return .failure(RemoteFeedLoader.Error.invalidData)
+		}
+		return .success([])
 	}
 }
