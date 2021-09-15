055d0719d943beab3fdc369169fe77c854213116
mohamed <mohamedibrahim1223@gmail.com>
2021-09-15 06:35:27 +0300

    Create File FeedImageMapper.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,7 +21,7 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { [weak self] result in
 			guard self != nil else { return }
-
+			
 			switch result {
 			case let .success((data, response)):
 				completion(FeedImageMapper.map(data, from: response))
@@ -29,37 +29,5 @@ public final class RemoteFeedLoader: FeedLoader {
 				completion(.failure(Error.connectivity))
 			}
 		}
-	}
-}
-
-final class FeedImageMapper {
-	private struct Root: Decodable {
-		let items: [Item]
-		var feeds: [FeedImage] {
-			return items.map { $0.item }
-		}
-	}
-
-	private struct Item: Decodable {
-		let image_id: UUID
-		let image_desc: String?
-		let image_loc: String?
-		let image_url: URL
-
-		var item: FeedImage {
-			return FeedImage(id: image_id,
-			                 description: image_desc,
-			                 location: image_loc,
-			                 url: image_url)
-		}
-	}
-
-	static func map(_ data: Data, from response: HTTPURLResponse) -> FeedLoader.Result {
-		guard response.statusCode == 200,
-		      let root = try? JSONDecoder().decode(Root.self, from: data)
-		else {
-			return .failure(RemoteFeedLoader.Error.invalidData)
-		}
-		return .success(root.feeds)
 	}
 }
